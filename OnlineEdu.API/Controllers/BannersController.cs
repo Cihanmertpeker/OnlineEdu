dc1325f6189ba5b469485644dd0b48fc843185b4
Cihan Mert PEKER <119748061+Cihanmertpeker@users.noreply.github.com>
2024-03-23 16:38:50 +0300

    Banners controller added

---
+++ OnlineEdu.API/Controllers/BannersController.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.Business.Abstract;
+using OnlineEdu.Dto.Dtos.AboutDtos;
+using OnlineEdu.Dto.Dtos.BannerDtos;
+using OnlineEdu.Entity.Entities;
+
+namespace OnlineEdu.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BannersController(IGenericService<Banner> _bannerService,IMapper _mapper) : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var values = _bannerService.TGetList();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var value = _bannerService.TGetById(id);
+            return Ok(value);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            _bannerService.TDelete(id);
+            return Ok("Değer Başarıyla Silindi");
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateBannerDto createBannerDto)
+        {
+            var newValue = _mapper.Map<Banner>(createBannerDto);
+            _bannerService.TCreate(newValue);
+            return Ok("Değer Başarıyla Oluşturuldu");
+        }
+
+        [HttpPut]
+        public IActionResult Update(UpdateBannerDto updateBannerDto)
+        {
+            var value = _mapper.Map<Banner>(updateBannerDto);
+            _bannerService.TUpdate(value);
+            return Ok("Değer Başarıyla Güncellendi");
+
+        }
+    }
+}
