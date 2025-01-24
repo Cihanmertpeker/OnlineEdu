dfe7a47f6c8b8ed573636adf4a3b6aa661f9878e
Cihan Mert PEKER <c.mertpeker@hotmail.com>
2025-01-24 21:12:44 +0300

    Add project files.

---
+++ OnlineEdu.DataAccess/Migrations/20250124175903_mig_bannerEntity_fixed.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineEdu.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_bannerEntity_fixed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Banners",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ImageUrl",
+                table: "Banners",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
