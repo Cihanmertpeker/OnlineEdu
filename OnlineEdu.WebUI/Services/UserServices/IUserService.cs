f96fedb931acf9472ea1df2dfa9106089e522802
Cihan Mert PEKER <c.mertpeker@hotmail.com>
2025-01-29 08:06:56 +0300

    -UserService and UserRegister -LoginPage theme

---
+++ OnlineEdu.WebUI/Services/UserServices/IUserService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using OnlineEdu.WebUI.DTOs.UserDtos;
+
+namespace OnlineEdu.WebUI.Services.UserServices
+{
+    public interface IUserService
+    {
+        Task<IdentityResult> CreateUserAsync(UserRegisterDto userRegisterDto);
+
+        Task<bool> LoginAsync(UserLoginDto userLoginDto);
+        Task<bool> LogoutAsync();
+
+        Task<bool> CreateRoleAsync(UserRoleDto userRoleDto);
+
+        Task<bool> AssignRoleAsync(List<AssignRoleDto> assignRoleDto);
+        //Task<List<UserViewModel>> GetAllUsersAsync();
+
+
+        //Task<List<AssignRoleDto>> GetUserForRoleAssign(int id);
+
+        //Task<int> GetTeacherCount();
+
+        //Task<List<ResultUserDto>> GetAllTeachers();
+
+    }
+}
---
+++ OnlineEdu.WebUI/Services/UserServices/UserService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity;
+using OnlineEdu.Entity.Entities;
+using OnlineEdu.WebUI.DTOs.UserDtos;
+
+namespace OnlineEdu.WebUI.Services.UserServices
+{
+    public class UserService(UserManager<AppUser> _userManager,SignInManager<AppUser> _signInManager,RoleManager<AppRole> _roleManager) : IUserService
+    {
+        public Task<bool> AssignRoleAsync(List<AssignRoleDto> assignRoleDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> CreateRoleAsync(UserRoleDto userRoleDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IdentityResult> CreateUserAsync(UserRegisterDto userRegisterDto)
+        {
+            var user = new AppUser
+            {
+                FirstName = userRegisterDto.FirstName,
+                LastName = userRegisterDto.LastName,
+                UserName = userRegisterDto.UserName,
+                Email = userRegisterDto.Email,
+            };
+            if (userRegisterDto.Password != userRegisterDto.ConfirmPassword)
+            {
+                return new IdentityResult();
+               
+            }
+            return await _userManager.CreateAsync(user, userRegisterDto.Password);
+       
+          
+        }
+
+        Task<bool> IUserService.LoginAsync(UserLoginDto userLoginDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<bool> IUserService.LogoutAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
