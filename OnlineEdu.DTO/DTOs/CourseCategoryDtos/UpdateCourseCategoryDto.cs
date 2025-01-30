f657c0585fdc22c0290b9dd68e7ab707d29a3f36
Cihan Mert PEKER <c.mertpeker@hotmail.com>
2025-01-30 17:10:42 +0300

    MyCourse Controller Create and update courses

--- OnlineEdu.DTO/DTOs/CourseCategoryDtos/UpdateCourseCategoryDto.cs
+++ OnlineEdu.DTO/DTOs/CourseCategoryDtos/UpdateCourseCategoryDto.cs
@@ -9,12 +9,11 @@ namespace OnlineEdu.DTO.DTOs.CourseCategoryDtos
 {
     public class UpdateCourseCategoryDto
     {
-
         public int CourseCategoryId { get; set; }
-        public string CategoryName { get; set; }
+        public string Name { get; set; }
         public string Icon { get; set; }
         public string Description { get; set; }
         public bool IsShown { get; set; }
-
+       
     }
 }
